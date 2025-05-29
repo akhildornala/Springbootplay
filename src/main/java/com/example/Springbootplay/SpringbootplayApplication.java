d99f85976103ccd05fb9b212977e504fbd8b6c37
akhildornala <dornala.akhil16july@gmail.com>
2025-05-28 23:50:37 -0700

    initial commit

---
+++ src/main/java/com/example/Springbootplay/SpringbootplayApplication.java
@@ -0,0 +1,13 @@
+package com.example.Springbootplay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootplayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootplayApplication.class, args);
+	}
+
+}
