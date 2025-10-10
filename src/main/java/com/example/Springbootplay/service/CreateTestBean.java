05b061333114152fc5dcd968cf260a804cdf1868
Akhil Dornala <32592689+akhildornala@users.noreply.github.com>
2025-10-10 01:14:22 -0700

    Update CreateTestBean.java

--- src/main/java/com/example/Springbootplay/service/CreateTestBean.java
+++ src/main/java/com/example/Springbootplay/service/CreateTestBean.java
@@ -8,6 +8,7 @@ import org.springframework.context.annotation.Configuration;
 public class CreateTestBean {
 
 
+  
 //    @Bean("firstbean")
 //    public Employee createEmployee1() {
 //        return new Employee();
