2506956524c548a801101725c4e59a3c44bc9204
akhildornala <dornala.akhil16july@gmail.com>
2025-10-18 20:32:49 -0700

    dummy commit

--- src/main/java/com/example/Springbootplay/service/CreateTestBean.java
+++ src/main/java/com/example/Springbootplay/service/CreateTestBean.java
@@ -8,7 +8,6 @@ import org.springframework.context.annotation.Configuration;
 public class CreateTestBean {
 
 
-
 //    @Bean("firstbean")
 //    public Employee createEmployee1() {
 //        return new Employee();
